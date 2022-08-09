07881270b4370f4ea2df75aca92fef6b3b217784
ashrafur22 <ashrafurrahman22@gmail.com>
2022-08-09 09:43:35 +0600

    connecting-with-github

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>Welcome to The homePage</h2>
+    <button class="btn btn-primary">Hello</button>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'Home'
+}
+</script>
